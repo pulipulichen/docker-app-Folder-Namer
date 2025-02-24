2163b31b2a902b7fb8b261a46bd942db947ec6c8
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-24 14:17:39 +0800

    西元2025年02月24日 (週一) 14時17分39秒 CST

--- app/folder-namer/extractFileLocation.js
+++ app/folder-namer/extractFileLocation.js
@@ -3,10 +3,11 @@ const getLocation = require('./getLocation')
 
 async function extractFileLocation(filePath) {
     const { latitude, longitude } = extractGPS(filePath);
-
+    console.log({ latitude, longitude })
     if (!latitude) {
         return null
     }
+
 
     const result = await getLocation(latitude, longitude)
     return result
