9e537b471afb59bd10409eb08a4c7e7b2659e44a
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-24 16:36:36 +0800

    西元2025年02月24日 (週一) 16時36分36秒 CST

--- app/folder-namer/askDify.js
+++ app/folder-namer/askDify.js
@@ -32,7 +32,7 @@ async function uploadFile(filePath, apiKey, user) {
     });
     formData.append('user', user);
 
-    console.log('不行嗎？')
+    console.log('不行嗎？', filePath)
 
     const response = await axios.post(
       'http://192.168.100.202/v1/files/upload',
