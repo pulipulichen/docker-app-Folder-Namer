a49498243da4d4f04248bef33a30a710d9144e87
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-24 16:38:44 +0800

    西元2025年02月24日 (週一) 16時38分44秒 CST

--- app/folder-namer/askDify.js
+++ app/folder-namer/askDify.js
@@ -40,7 +40,7 @@ async function uploadFile(filePath, apiKey, user) {
       {
         headers: {
           Authorization: `Bearer ${API_KEY}`,
-          // ...formData.getHeaders(),
+          ...formData.getHeaders(),
         },
       }
     );
