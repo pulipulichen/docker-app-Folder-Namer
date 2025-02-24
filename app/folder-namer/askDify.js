9f249f577fcec25916ebd9168142e1ba842ee31c
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-24 17:44:56 +0800

    西元2025年02月24日 (週一) 17時44分56秒 CST

--- app/folder-namer/askDify.js
+++ app/folder-namer/askDify.js
@@ -74,7 +74,7 @@ async function executeWorkflow(document_id, apiKey, user) {
       {
         inputs: {
           context: '測試',
-          'file': [{
+          'image': [{
             type: "image",
             "transfer_method": "local_file",
             "upload_file_id": document_id
